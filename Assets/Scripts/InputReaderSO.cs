ee68c50b1185aedea8168cbfb4554b87260087f5
lucasFrazao <lucas_devfrazao@outlook.com>
2025-03-28 21:45:32 -0300

    Implemented main menu UI

--- Assets/Scripts/InputReaderSO.cs
+++ Assets/Scripts/InputReaderSO.cs
@@ -3,88 +3,121 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "InputReader", menuName = "Scriptable Objects/InputReader")]
-public class InputReaderSO : ScriptableObject, PlayerInputController.IPlayerActions
+public class InputReaderSO : ScriptableObject, PlayerInputController.IPlayerActions, PlayerInputController.IUIActions
 {
+    // Gameplay Events
     public event Action OnJumpPerformed;
     public event Action OnRunPerformed;
     public event Action OnAttackPerformed;
     public event Action OnShieldDefensePerformed;
 
-    private PlayerInputController m_playerInput;
+    // UI Events
+    public event Action<Vector2> OnNavigatePerformed;
+    public event Action OnUISubmitPerformed;
+    public event Action OnUICancelPerformed;
 
-    // Inicialização
+    private PlayerInputController m_playerInput;
+    private bool m_isInUIMode = false;
+
+    public bool IsInUIMode => m_isInUIMode;
+
     private void OnEnable()
     {
         if (m_playerInput == null)
         {
             m_playerInput = new PlayerInputController();
             m_playerInput.Player.SetCallbacks(this);
+            m_playerInput.UI.SetCallbacks(this);
 
-            // Registra os eventos
-            m_playerInput.Player.Jump.performed += OnJump;
-            m_playerInput.Player.Run.performed += OnRun;
-            m_playerInput.Player.ShieldDefense.performed += OnShieldDefense;
-
-            m_playerInput.Player.Enable();
+            EnableUIMode();
         }
-    }
-
-    public Vector2 GetMovementVectorNormalized()
-    {
-        Vector2 inputVector = m_playerInput.Player.Move.ReadValue<Vector2>();
-        return inputVector.normalized;
-    }
-
-    public Vector2 GetLookVector()
-    {
-        Vector2 inputVector = m_playerInput.Player.Look.ReadValue<Vector2>();
-        return inputVector.normalized;
     }
 
     private void OnDisable()
     {
         if (m_playerInput != null)
         {
-            // Remove os eventos
             m_playerInput.Player.Jump.performed -= OnJump;
             m_playerInput.Player.Run.performed -= OnRun;
             m_playerInput.Player.ShieldDefense.performed -= OnShieldDefense;
+
+            m_playerInput.UI.Navigate.performed -= OnNavigate;
+            m_playerInput.UI.Submit.performed -= OnSubmit;
+            m_playerInput.UI.Cancel.performed -= OnCancel;
 
             m_playerInput.Disable();
         }
     }
 
+    public void EnableUIMode()
+    {
+        m_playerInput.Player.Disable();
+        m_playerInput.UI.Enable();
+        m_isInUIMode = true;
+    }
+
+    public void EnableGameplayMode()
+    {
+        m_playerInput.UI.Disable();
+        m_playerInput.Player.Enable();
+        m_isInUIMode = false;
+    }
+
+    // Gameplay Actions
     public void OnJump(InputAction.CallbackContext context)
     {
-        OnJumpPerformed?.Invoke();
+        if (!m_isInUIMode && context.performed)
+            OnJumpPerformed?.Invoke();
     }
 
     public void OnRun(InputAction.CallbackContext context)
     {
-        OnRunPerformed?.Invoke();
+        if (!m_isInUIMode && context.performed)
+            OnRunPerformed?.Invoke();
     }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (!m_isInUIMode && context.performed)
             OnAttackPerformed?.Invoke();
     }
 
     public void OnShieldDefense(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (!m_isInUIMode && context.performed)
             OnShieldDefensePerformed?.Invoke();
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    // UI Actions
+    public void OnNavigate(InputAction.CallbackContext context)
     {
-        // Method exists because interface demands
-        // Move read values in GetMovementVectorNormalized()
+        //if (m_isInUIMode && context.performed)
+        //    OnNavigatePerformed?.Invoke(context.ReadValue<Vector2>());
     }
 
-    public void OnLook(InputAction.CallbackContext context)
+    public void OnSubmit(InputAction.CallbackContext context)
     {
-        // Method exists because interface demands
-        // Look read values in CameraController script
+        //if (m_isInUIMode && context.performed)
+        //    OnUISubmitPerformed?.Invoke();
     }
+
+    public void OnCancel(InputAction.CallbackContext context)
+    {
+        if (m_isInUIMode && context.performed)
+            OnUICancelPerformed?.Invoke();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        return m_isInUIMode ? Vector2.zero : m_playerInput.Player.Move.ReadValue<Vector2>().normalized;
+    }
+
+    public Vector2 GetLookVector()
+    {
+        return m_isInUIMode ? Vector2.zero : m_playerInput.Player.Look.ReadValue<Vector2>().normalized;
+    }
+
+    // Métodos da interface (não utilizados)
+    public void OnMove(InputAction.CallbackContext context) { }
+    public void OnLook(InputAction.CallbackContext context) { }
 }
